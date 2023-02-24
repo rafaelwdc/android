3f0be7f4a9ce27b069174a195dc8638607ac3225
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-24 02:49:22 -0300

    Detalhes da cesta -> 02. Componentes e estilo => React Native: criando um app

--- orgs-cesta/src/telas/Cesta.js
+++ orgs-cesta/src/telas/Cesta.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +9,16 @@ export default function Cesta () {
     return <>
         <Image source={topo} style={estilos.topo} />
         <Text style={estilos.titulo}>Detalhe da cesta</Text>
+
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <View style={estilos.fazenda}>
+                <Image style={estilos.imagemFazenda} source={logo} />
+                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={estilos.descricao}>Uma cesta com produtos selecionados, cuidadosamente da fazenda direto para sua cozina </Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
@@ -25,5 +36,41 @@ const estilos = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
         padding: 16
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold'
+    },
+    fazenda: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 16,
+        marginLeft: 12
+    },
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
-})
+});
