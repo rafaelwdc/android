f657490d790b537c8f10d3f5147721de7385ad0a
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-25 19:08:22 -0300

    Preparando o ambiente: Lista de itens -> 04. Botão e lista => React Native: criando um app

--- orgs-cesta/src/mocks/cestas.js
+++ orgs-cesta/src/mocks/cestas.js
@@ -1,5 +1,11 @@
 import logo from '../../assets/logo.png';
 
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
+
 const cesta = {
     topo: {
         titulo: "Detalhe da cesta",
@@ -10,6 +16,31 @@ const cesta = {
         nomeFazenda: "Jane Jack Farm",
         descricao: "Uma cesta com produtos selecionados, cuidadosamente da fazenda direto para sua cozinha",
         preco: "R$ 40,00"
+    },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                imagem: tomate
+            },
+            {
+                nome: "Brocolis",
+                imagem: brocolis
+            },
+            {
+                nome: "Batata",
+                imagem: batata
+            },
+            {
+                nome: "Pepino",
+                imagem: pepino
+            },
+            {
+                nome: "Abobora",
+                imagem: abobora
+            }
+        ]
     }
 }
 
