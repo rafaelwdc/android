87639c762bc5f2c43ddf6cccfc5351468c36023a
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-25 00:14:50 -0300

    SplashScreen e revisão -> 03. Refatorando => React Native: criando um app

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import { 
   useFonts, 
   Montserrat_400Regular, 
   Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat';
+import * as SplashScreen from 'expo-splash-screen'
 
 import Cesta from './src/telas/Cesta';
-import mock from './src/mocks/cestas'
+import mock from './src/mocks/cestas';
 
 export default function App() {
 
@@ -15,13 +16,29 @@ export default function App() {
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold,
   });
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
 
-  if(!fonteCarregada) {
-    return <View />
+  const onLayoutRootView = useCallback(async () => {
+    if (fonteCarregada) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fonteCarregada]);
+
+  if (!fonteCarregada) {
+    return null;
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}  onLayout={onLayoutRootView}>
       <StatusBar />
       <Cesta {...mock} />
     </SafeAreaView>
