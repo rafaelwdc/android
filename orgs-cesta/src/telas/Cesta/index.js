c87fcdb72afd584e8abc6888b07bb70ab6926c2e
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-25 20:46:18 -0300

    Componente de lista -> 04. Botão e lista => React Native: criando um app

--- orgs-cesta/src/telas/Cesta/index.js
+++ orgs-cesta/src/telas/Cesta/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import Detalhes from "./componentes/Detalhes";
+import Itens from "./componentes/Itens";
 import Topo from "./componentes/Topo";
 
-export default function Cesta ({ topo, detalhes }) {
-    return <>
+export default function Cesta ({ topo, detalhes, itens }) {
+    return <ScrollView>
         <Topo {...topo} />
 
         <View style={estilos.cesta}>
             <Detalhes {...detalhes} />
+            <Itens {...itens} />
         </View>
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
