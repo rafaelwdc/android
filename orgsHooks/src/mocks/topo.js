dfac3a06f2876c56a17ba6b2f140c69b7670acc3
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-26 02:46:50 -0300

    Componente do topo && Preparando o ambiente: Arquivo de Mock -> 02. Estados e ciclo de vida => React Native: ultilizando e criando Hooks

---
+++ orgsHooks/src/mocks/topo.js
@@ -0,0 +1,9 @@
+import logo from '../assets/logo.png';
+
+const topo = {
+    logo: logo,
+    boasVindas: "Olá Natalia",
+    legenda: "Encontre os melhores produtores",
+};
+
+export default topo;
