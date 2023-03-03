1cc42c2150615dcadd7d7235421d30c614f5095a
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-03-03 02:18:57 -0300

    Adicionando os arquivo do Produtor // 03. Navegando para o Produtor -> React Native: criando menu e navegando entre telas

---
+++ orgsnavegacao/src/telas/Produtor/estilos.js
@@ -0,0 +1,33 @@
+const estilos = StyleSheet.create({
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    conteudo: {
+        paddingHorizontal: 16,
+    },
+    logo: {
+        flexDirection: 'row',
+    },
+    produtorImage: {
+        width: 62,
+        height: 62,
+
+        marginTop: -23,
+
+        borderRadius: 6,
+    },
+    produtor: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginLeft: 16,
+    },
+    cestas: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginTop: 32,
+    }
+});
