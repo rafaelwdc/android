ed1f09c2641bda18a80855868e6228545f3930ac
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-03-03 02:15:53 -0300

    Envio de dados ao Produtor // 03. Navegando para o Produtor -> React Native: criando menu e navegando entre telas

--- orgsnavegacao/src/telas/Produtor/index.js
+++ orgsnavegacao/src/telas/Produtor/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useRoute } from '@react-navigation/native';
 import { Text } from 'react-native';
 
 export default function Produtor() {
+  const route = useRoute();
+  console.log(route.params);
+
   return <Text>Produtor</Text>
 
 }
